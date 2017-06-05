3fb692de22c4d7073222a99f6852ad1337d89f45
nature <2726916340@qq.com>
2017-06-05 22:04:30 +0800

    The first Version for file System project

---
+++ Assets/Scripts/NewFile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewFile : MonoBehaviour {
+    public Button File;
+    private Canvas Background;
+    private Vector3 position;
+    private Quaternion rotation;
+
+	// Use this for initialization
+	void Start () {
+        position = new Vector3(-200, 165, 0);
+        rotation = File.transform.rotation;
+        Background = GameObject.Find("Canvas").GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        Button newFile = Instantiate(File, position, rotation,Background.transform);
+        newFile.transform.localPosition = position;
+
+        newFile.GetComponentInChildren<Text>().text = "File1";
+    }
+}
